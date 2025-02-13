f03e1cdf823a8dec5e65feee7d3000eda4529341
Gibrel Ollokut <gibrel@gmail.com>
2025-02-13 10:28:51 -0300

    configuring tailwindcss 4 over primevue

--- frontend/README.md
+++ frontend/README.md
@@ -3,8 +3,8 @@
 ```shell
 npm create vite@latest .
 npm i
-npm i primevue primeicons tailwindcss-primeui axios
-npm i -D tailwindcss postcss autoprefixer @primevue/auto-import-resolver unplugin-vue-components vite-plugin-vue-devtools @tsconfig/node20 @types/node 
+npm i primevue primeicons tailwindcss-primeui @tailwindcss/vite tailwindcss
+npm i -D @primevue/auto-import-resolver unplugin-vue-components vite-plugin-vue-devtools @tsconfig/node20 @types/node @tailwindcss/postcss
 ```
 
 ## Install
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
@@ -11,9 +12,10 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
     Components({
       resolvers: [PrimeVueResolver()]
-    })
+    }),
   ],
   resolve: {
     alias: {
